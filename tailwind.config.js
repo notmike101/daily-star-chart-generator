b4f4a732b3d27be24904dd10332d5ada34e0515a
Mike Orozco <me@mikeorozco.dev>
2023-02-20 20:09:13 -0600

    Remove unused tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-const { nthChildPlugin } = require('./tailwind/nthChildPlugin');
-
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts}',
   ],
   theme: {},
-  plugins: [
-    nthChildPlugin,
-  ],
+  plugins: [],
 }
